f8e6237ee45de395d698c081cbf75b55f617063c
Adam Andersson <adam.ottvar@gmail.com>
2017-05-07 15:17:03 +0200

    Working label update
    Updates labels with dummy-time (current time plus minutes to see
    difference).

--- depature_board.py
+++ depature_board.py
@@ -1,4 +1,6 @@
+import datetime
 from tkinter import *
+
 UPPER_DEPATURE = "Redbergsplatsen"
 UPPER_ARRIVAL = "Östra Sjukhuset"
 LOWER_DEPATURE = "Olskrokstorget"
@@ -29,18 +31,38 @@ class TramRow:
     """
     def __init__(self, tram_frame, tram_number, tram_description, tram_dep_time):
         self.frame = Frame(tram_frame, bg = "black")
-        self.number = tram_number
-        self.descr = tram_description
-        self.depTime = tram_dep_time
-        self.numberLabel = Label(self.frame, text=self.number, font=("default",FONT_SIZE_TRAM), bg = "black", fg="white")
+        self.number = StringVar()
+        self.descr = StringVar()
+        self.depTime = StringVar()
+
+
+        self.number.set(tram_number)
+        self.descr.set(tram_description)
+        self.depTime.set(tram_dep_time)
+        self.numberLabel = Label(self.frame, textvariable=self.number, font=("default",FONT_SIZE_TRAM), bg = "black", fg="white")
         self.numberLabel.pack(side="left", fill="y", padx=10)
-        self.descrLabel = Label(self.frame, text=self.descr, font=("default",FONT_SIZE_TRAM), bg = "black", fg="white")
+        self.descrLabel = Label(self.frame, textvariable=self.descr, font=("default",FONT_SIZE_TRAM), bg = "black", fg="white")
         self.descrLabel.pack(side="left", fill="both")
-        self.timeLabel = Label(self.frame, text=self.depTime, font=("default",FONT_SIZE_TRAM), bg = "black", fg="white")
-        self.timeLabel.pack(side="right", fill="y")
+        self.timeLabel = Label(self.frame, textvariable=self.depTime, font=("default",FONT_SIZE_TRAM), bg = "black", fg="white")
+        self.timeLabel.pack(side="right", fill="y", padx=10)
 
         self.frame.pack(side="top", fill="both", pady=10)
         
+
+def update_time():
+    _tram_datetime = datetime.datetime.now()+datetime.timedelta(minutes = 0)
+    _tram_time = _tram_datetime.strftime('%H:%M')
+    upperContentRow1.depTime.set(_tram_time)
+    _tram_datetime = datetime.datetime.now()+datetime.timedelta(minutes = 1)
+    _tram_time = _tram_datetime.strftime('%H:%M')
+    upperContentRow2.depTime.set(_tram_time)
+    _tram_datetime = datetime.datetime.now()+datetime.timedelta(minutes = 2)
+    _tram_time = _tram_datetime.strftime('%H:%M')
+    lowerContentRow1.depTime.set(_tram_time)
+    _tram_datetime = datetime.datetime.now()+datetime.timedelta(minutes = 3)
+    _tram_time = _tram_datetime.strftime('%H:%M')
+    lowerContentRow2.depTime.set(_tram_time)
+    root.after(2000, update_time)
 
 
 root = Tk()
@@ -48,25 +70,43 @@ root.title("Departure Board")
 
 # Upper Title
 upperTitleFrame = Frame(root, bg = "black")
-upperTitleFrame.pack(side="top", fill="both", pady=10)
+upperTitleFrame.pack(side="top", fill="both")
 upperTitleRow = TitleRow(upperTitleFrame, UPPER_DEPATURE, UPPER_ARRIVAL)
 
 # Upper Content
 upperContentFrame = Frame(root, bg = "black")
-upperContentFrame.pack(side="top", fill="both", expand=True, pady=10)
+upperContentFrame.pack(side="top", fill="both", expand=True)
 upperContentRow1 = TramRow(upperContentFrame, "1", "Östra sjukhuset", "12:34")
 upperContentRow2 = TramRow(upperContentFrame, "1", "Östra sjukhuset", "12:34")
 
 # Lower Title
 lowerTitleFrame = Frame(root, bg = "black")
-lowerTitleFrame.pack(side="top", fill="both", pady=10)
+lowerTitleFrame.pack(side="top", fill="both")
 lowerTitleLabel = TitleRow(lowerTitleFrame, LOWER_DEPATURE, LOWER_ARRIVAL)
 
 # Lower Content
 lowerContentFrame = Frame(root, bg = "black")
-lowerContentFrame.pack(side="top", fill="both", expand=True, pady=10)
+lowerContentFrame.pack(side="top", fill="both", expand=True)
 lowerContentRow1 = TramRow(lowerContentFrame, "8", "Frölunda", "13:37")
 lowerContentRow2 = TramRow(lowerContentFrame, "8", "Frölunda", "13:37")
 
 
+def update_time():
+    _tram_datetime = datetime.datetime.now()+datetime.timedelta(minutes = 0)
+    _tram_time = _tram_datetime.strftime('%H:%M')
+    upperContentRow1.depTime.set(_tram_time)
+    _tram_datetime = datetime.datetime.now()+datetime.timedelta(minutes = 1)
+    _tram_time = _tram_datetime.strftime('%H:%M')
+    upperContentRow2.depTime.set(_tram_time)
+    _tram_datetime = datetime.datetime.now()+datetime.timedelta(minutes = 2)
+    _tram_time = _tram_datetime.strftime('%H:%M')
+    lowerContentRow1.depTime.set(_tram_time)
+    _tram_datetime = datetime.datetime.now()+datetime.timedelta(minutes = 3)
+    _tram_time = _tram_datetime.strftime('%H:%M')
+    lowerContentRow2.depTime.set(_tram_time)
+    root.after(2000, update_time)
+
+root.after(0, update_time)
 root.mainloop()
+
+
