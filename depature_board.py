2641f0fae22d4ee64dda8fe455c112b44b7ae20c
Adam Andersson <adam.ottvar@gmail.com>
2017-05-07 14:10:02 +0200

    Added classes for title and tram
    Added classes to create title rows and tram rows.

--- depature_board.py
+++ depature_board.py
@@ -3,39 +3,70 @@ UPPER_DEPATURE = "Redbergsplatsen"
 UPPER_ARRIVAL = "Östra Sjukhuset"
 LOWER_DEPATURE = "Olskrokstorget"
 LOWER_ARRIVAL = "Svingeln"
+FONT_SIZE_TITLE = 40
+FONT_SIZE_TRAM = 30
+
+class TitleRow:
+    """Class for creating a row with the title och the frame
+
+    The title is "the departure station" + "mot" + "the arrival station"
+    """
+    def __init__(self, title_frame, dep_station, arr_station):
+        self.frame = title_frame
+        self.departure = dep_station
+        self.arrival = arr_station
+
+        self.titleLabel = Label(self.frame, text=self.departure + " mot " + self.arrival,
+                        font=("default",30), bg = "black", fg="white")
+        self.titleLabel.pack(side="top", fill="both", expand=True, pady=10)
+
+
+class TramRow:
+    """Class for creating a row with tram information
+
+    Tram information is stored in a frame with three labels that store
+    the tram number, tram description and tram departure time.
+    """
+    def __init__(self, tram_frame, tram_number, tram_description, tram_dep_time):
+        self.frame = Frame(tram_frame, bg = "black")
+        self.number = tram_number
+        self.descr = tram_description
+        self.depTime = tram_dep_time
+        self.numberLabel = Label(self.frame, text=self.number, font=("default",FONT_SIZE_TRAM), bg = "black", fg="white")
+        self.numberLabel.pack(side="left", fill="y", padx=10)
+        self.descrLabel = Label(self.frame, text=self.descr, font=("default",FONT_SIZE_TRAM), bg = "black", fg="white")
+        self.descrLabel.pack(side="left", fill="both")
+        self.timeLabel = Label(self.frame, text=self.depTime, font=("default",FONT_SIZE_TRAM), bg = "black", fg="white")
+        self.timeLabel.pack(side="right", fill="y")
+
+        self.frame.pack(side="top", fill="both", pady=10)
+        
 
 
 root = Tk()
 root.title("Departure Board")
 
-UpperTitleFrame = Frame(root, bg = "black")
-UpperTitleFrame.pack(side="top", fill="both")
-UpperTitleLabel = Label(UpperTitleFrame, text=UPPER_DEPATURE + " mot " + UPPER_ARRIVAL,
-                        font=("default",30), bg = "black", fg="white")
-UpperTitleLabel.pack(side="top", fill="both", expand=True)
+# Upper Title
+upperTitleFrame = Frame(root, bg = "black")
+upperTitleFrame.pack(side="top", fill="both", pady=10)
+upperTitleRow = TitleRow(upperTitleFrame, UPPER_DEPATURE, UPPER_ARRIVAL)
 
-UpperContentFrame = Frame(root, bg = "black")
-UpperContentFrame.pack(side="top", fill="both")
-UpperContentNumber = Label(UpperContentFrame, text="1", font=("default",30), bg = "black", fg="white")
-UpperContentNumber.pack(side="left", fill="y")
-UpperContentDescr = Label(UpperContentFrame, text="Östra Sjukhuset", font=("default",30), bg = "black", fg="white")
-UpperContentDescr.pack(side="left", fill="both")
-UpperContentTime = Label(UpperContentFrame, text="12:34", font=("default",30), bg = "black", fg="white")
-UpperContentTime.pack(side="right", fill="y")
+# Upper Content
+upperContentFrame = Frame(root, bg = "black")
+upperContentFrame.pack(side="top", fill="both", expand=True, pady=10)
+upperContentRow1 = TramRow(upperContentFrame, "1", "Östra sjukhuset", "12:34")
+upperContentRow2 = TramRow(upperContentFrame, "1", "Östra sjukhuset", "12:34")
 
-LowerTitleFrame = Frame(root, bg = "black")
-LowerTitleFrame.pack(side="top", fill="both")
-LowerTitleLabel = Label(LowerTitleFrame, text=LOWER_DEPATURE + " mot " + LOWER_ARRIVAL,
-                        font=("default",30), bg = "black", fg="white")
-LowerTitleLabel.pack(side="top", fill="both", expand=True)
+# Lower Title
+lowerTitleFrame = Frame(root, bg = "black")
+lowerTitleFrame.pack(side="top", fill="both", pady=10)
+lowerTitleLabel = TitleRow(lowerTitleFrame, LOWER_DEPATURE, LOWER_ARRIVAL)
 
-LowerContentFrame = Frame(root, bg = "black")
-LowerContentFrame.pack(side="top", fill="both")
-LowerContentNumber = Label(LowerContentFrame, text="8", font=("default",30), bg = "black", fg="white")
-LowerContentNumber.pack(side="left", fill="y")
-LowerContentDescr = Label(LowerContentFrame, text="Frölunda", font=("default",30), bg = "black", fg="white")
-LowerContentDescr.pack(side="left", fill="both")
-LowerContentTime = Label(LowerContentFrame, text="13:37", font=("default",30), bg = "black", fg="white")
-LowerContentTime.pack(side="right", fill="y")
+# Lower Content
+lowerContentFrame = Frame(root, bg = "black")
+lowerContentFrame.pack(side="top", fill="both", expand=True, pady=10)
+lowerContentRow1 = TramRow(lowerContentFrame, "8", "Frölunda", "13:37")
+lowerContentRow2 = TramRow(lowerContentFrame, "8", "Frölunda", "13:37")
+
 
 root.mainloop()
